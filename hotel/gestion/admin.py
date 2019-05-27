23d63014830c2eca10b10dc353c63c14d4c1fd53
Ziiiixi <chenzixi0529@gmail.com>
2019-05-27 16:41:07 +0200

    5.27

--- hotel/gestion/admin.py
+++ hotel/gestion/admin.py
@@ -10,5 +10,13 @@ class ressourceAdmin(admin.ModelAdmin):
 class demandeAdmin(admin.ModelAdmin):
     list_display = ('id', 'checkin', 'checkout','nbPerson', 'client','status')  # list
 
+class MeubleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom_Meuble','status')  # list
+
+class Concerne_MeubleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ressource', 'meuble')  # list
+
 admin.site.register(Ressource,ressourceAdmin)
 admin.site.register(Demande,demandeAdmin)
+admin.site.register(Meuble,MeubleAdmin)
+admin.site.register(Concerne_Meuble,Concerne_MeubleAdmin)
--- hotel/gestion/models.py
+++ hotel/gestion/models.py
@@ -1,5 +1,8 @@
 from login.models import *
 # Create your models here.
+class Meuble(models.Model):
+    nom_Meuble =  models.CharField(max_length=32, default="default")
+    status = models.CharField(max_length=20, default="bon")
 
 class Ressource(models.Model):
     numero = models.CharField(max_length=32, default="default")
@@ -9,11 +12,15 @@ class Ressource(models.Model):
     def __unicode__(self):
         return self.numero
 
+class Concerne_Meuble(models.Model):
+    ressource = models.ForeignKey(Ressource, default = None, on_delete=models.CASCADE)
+    meuble = models.ForeignKey(Meuble, default = None, on_delete=models.CASCADE)
+
 
 class Demande(models.Model):
     demandeTime = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     checkin = models.DateField()
     checkout = models.DateField()
     nbPerson = models.IntegerField(default=2)
-    client = models.ForeignKey(Client,on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, default="attendu")
